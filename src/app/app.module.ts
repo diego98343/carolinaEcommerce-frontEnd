182a65e499b953c5fae524c652ec4771f2dcc3ad
diegocolonia <diegocolonia98@yahoo.com>
2022-11-06 22:32:46 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { ProductsComponent } from './products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+
+const routers: Routes=[
+  {path:'home',component: FrontPageComponent},
+  {path:'products',component: ProductsComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    ProductsComponent
   ],
-  imports: [
+  imports: [ AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routers)
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
