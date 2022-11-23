bed80f2e24a6809042e9198a6b113499751c321d
diegocolonia <diegocolonia98@yahoo.com>
2022-11-23 01:42:18 -0500

    commit

--- src/app/tryout/try/try.component.ts
+++ src/app/tryout/try/try.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductClass } from 'src/app/models/product-class';
 import { ProductCategory } from 'src/app/models/productCategoryModel/product-category';
+import { ProductServiceService } from 'src/app/services/product-service.service';
 import { ProductCategoryService } from 'src/app/services/productCategoryService/product-category.service';
 
 @Component({
@@ -11,13 +13,21 @@ export class TryComponent implements OnInit {
 
   productCategories: ProductCategory[]=[];
 
+  products: ProductClass[]=[];
+
   
 
-  constructor( private _productCategoryService: ProductCategoryService) { }
+  constructor( private _productCategoryService: ProductCategoryService,
+               private _productService: ProductServiceService
+               ) { }
 
   ngOnInit(): void {
     this._productCategoryService.getCategories().subscribe(
       data=>this.productCategories=data
+    )
+
+    this._productService.getProduct().subscribe(
+      data=> this.products=data
     )
   }
 
