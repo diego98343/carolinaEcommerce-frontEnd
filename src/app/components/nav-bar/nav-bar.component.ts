853f18970fa28592778ccf52c6a759c5a40c7d59
diegocolonia <diegocolonia98@yahoo.com>
2023-01-08 23:13:31 -0500

    commit

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartServiceService } from 'src/app/services/cartService/cart-service.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+
+   totalQuantity: number =0;
+
+
+  constructor(private _cartService:CartServiceService) { }
 
   ngOnInit(): void {
+
+    this.updataCartComponent();
   }
+
+
+updataCartComponent(){
+  this._cartService.totalQuantity.subscribe(data=>{
+    this.totalQuantity=data;
+  })
+}
+  
 
 }
