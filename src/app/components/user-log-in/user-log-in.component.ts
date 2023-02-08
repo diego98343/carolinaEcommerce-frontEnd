e4d0c501ca53daf1ba3ab731249fcd712329a12e
diegocolonia <diegocolonia98@yahoo.com>
2023-02-07 23:58:31 -0500

    commit

--- src/app/components/user-log-in/user-log-in.component.ts
+++ src/app/components/user-log-in/user-log-in.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { User } from 'src/app/models/userClass/user';
 import { LogINService } from 'src/app/services/logInService/log-in.service';
+
+import myAppConfig from 'src/app/config/my-app-config';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'app-user-log-in',
@@ -9,9 +13,15 @@ import { LogINService } from 'src/app/services/logInService/log-in.service';
 })
 export class UserLogInComponent implements OnInit {
 
+
+
   user:User=new User();
 
-  constructor(private userService:LogINService) { }
+  oktaSignin:any;
+
+
+  constructor(private userService:LogINService,
+              @Inject(OKTA_AUTH)private oktaAuth:OktaAuth) { }
 
   ngOnInit(): void {
     // this.userLogIn();
